651b39af91e0d87f4d74a67e33662513993e4cc9
Joseph Johnson <johnson_jt@outlook.com>
2021-12-13 15:44:32 -0500

    Add code for isClassProfiled() and wrap()

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -3,6 +3,9 @@ package com.udacity.webcrawler.profiler;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.nio.file.Path;
 import java.time.Clock;
 import java.time.ZonedDateTime;
@@ -25,16 +28,32 @@ final class ProfilerImpl implements Profiler {
     this.startTime = ZonedDateTime.now(clock);
   }
 
-  @Override
-  public <T> T wrap(Class<T> klass, T delegate) {
-    Objects.requireNonNull(klass);
+  private boolean isClassProfiled(Class<?> klazz) {
+    Method[] methods = klazz.getDeclaredMethods();
+    if (methods.length == 0) {return false;}
+    for (Method method : methods) {
+      if (method.getAnnotation(Profiled.class) != null) {
+        return true;
+      }
+    }
+    return false;
+  }
 
+  @Override
+  public <T> T wrap(Class<T> klass, T delegate) throws IllegalArgumentException{
+    Objects.requireNonNull(klass);
+    if (!isClassProfiled(klass)) {
+      throw new IllegalArgumentException("No profiled methods found");
+    }
+    InvocationHandler handler = new ProfilingMethodInterceptor(clock, delegate, state);
+
+    T proxy = (T) Proxy.newProxyInstance(klass.getClassLoader(), new Class[]{klass}, handler );
+
+    return proxy;
+    }
     // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
-
-    return delegate;
-  }
 
   @Override
   public void writeData(Path path) {
